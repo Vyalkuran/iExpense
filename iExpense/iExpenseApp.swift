720fc4994a98412ea922e45958808556ffa0da3b
sebastian.popa <sebastian.popa@softvision.com>
2023-08-07 18:52:22 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by sebastian.popa on 8/7/23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
